929dca9d714bb2f52a79ef03653da1da613b4412
hossein0402 <h.ahmadi0402@gmail.com>
2026-08-11 14:37:30 +0330

    add some configurations classes

---
+++ src/main/java/org/example/job_builder/config/FlinkConfiguration.java
@@ -0,0 +1,31 @@
+package org.example.job_builder.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+
+@Getter
+@Configuration
+public class FlinkConfiguration {
+
+    @Value("${flink.job-manager.host}")
+    private String jobManagerHost;
+
+    @Value("${flink.job-manager.port}")
+    private int jobManagerPort;
+
+    @Value("${flink.job-jar-path}")
+    private String jobJarPath;
+
+    @Value("${flink.redis-host-in-cluster}")
+    private String redisHostInCluster;
+
+    @Value("${flink.redis-port-in-cluster}")
+    private String redisPortInCluster;
+
+    public String flinkBaseUrl() {
+        return String.format("http://%s:%d", jobManagerHost, jobManagerPort);
+    }
+
+}
